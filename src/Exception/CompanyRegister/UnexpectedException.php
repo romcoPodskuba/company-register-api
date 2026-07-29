a2abbabbf514f8ebe8f5d306ac46ba852ebba9f2
Romco <roman.podskuba@gmail.com>
2026-07-29 11:33:36 +0200

    UnexpectedException typo fixed

--- src/Exception/CompanyRegister/UnexpectedException.php
+++ src/Exception/CompanyRegister/UnexpectedException.php
@@ -6,4 +6,4 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\WithHttpStatus;
 
 #[WithHttpStatus(Response::HTTP_INTERNAL_SERVER_ERROR)]
-class UnexcepectedException extends \RuntimeException {}
+class UnexpectedException extends \RuntimeException {}
