cf8fe4247353a6be4c1ca4f4661348aa49ceb1fa
Romco <roman.podskuba@gmail.com>
2026-07-29 14:44:13 +0200

    Implement CompanyRegisterProviderInterface in CompanyService and update services configuration

--- src/Service/CompanyService.php
+++ src/Service/CompanyService.php
@@ -3,11 +3,16 @@
 namespace App\Service;
 
 use App\DTO\CompanyRegister\Company;
+use App\Provider\CompanyRegister\CompanyRegisterProviderInterface;
 
 class CompanyService
 {
+    public function __construct(
+        private readonly CompanyRegisterProviderInterface $companyRegisterProvider
+    ) {}
+
     public function getFromRegister(string $businessId): Company
     {
-        //todo
+        return $this->companyRegisterProvider->getCompany($businessId);
     }
 }
